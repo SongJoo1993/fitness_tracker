fd7d796d238bc7a477b514628c95666fb5f4cd1d
Song Joo <songjoo29@gmail.com>
2023-11-04 16:13:26 -0400

    mongodb connection done and API file WIP

---
+++ src/pages/api/user/index.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import clientPromise from "../../../../database/mongodb";
+// import { ObjectId } from "mongodb";
+
+export const getAllUsers = async (): Promise<{}> => {
+  const mongoClient = await clientPromise;
+  const data = await mongoClient
+    .db("fitness_app")
+    .collection("users")
+    .find({})
+    .toArray();
+
+  return JSON.parse(JSON.stringify(data));
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const data = await getAllUsers();
+    res.json({ tags: data });
+  } 
+}
